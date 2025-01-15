de574c8dc7c6c5d30b3f3eec410d47ae01d88507
Adam Herro <adam.herro@comcast.net>
2025-01-14 20:51:47 -0500

    Creating function to make a new directory.

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ import fs from 'fs'
 
 // Create a new directory 
 
+async function makeDir() {
+    const newDir = await fs.mkdir('new-project', () => {
+        try {
+            console.log(newDir)
+            return newDir
+    
+        } catch (error) {
+            if (error) throw error
+        }
+    })
+}
+makeDir()
 
 // Push a new file to directory
 
 
 // Write to file
-let data = ''
-fs.writeFile('./project/index.html', data, 'utf8', (err) => {
-    if(err) throw err;
+// let data = ''
+// fs.writeFile('./project/index.html', data, 'utf8', (err) => {
+//     if(err) throw err;
 
-    // Print out data added to (file name)
-    console.log('Data added to file')
-})
+//     // Print out data added to (file name)
+//     console.log('Data added to file')
+// })
